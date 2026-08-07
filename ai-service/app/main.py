50287019e17bbbd571db1934a5302a70d606a04c
PraneethKandukuriii <praneethkandukuriii@gmail.com>
2026-08-08 04:05:10 +0530

    Add CORS for frontend connection

--- ai-service/app/main.py
+++ ai-service/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes import upload, chat
 
@@ -6,6 +7,15 @@ from app.routes import upload, chat
 app = FastAPI(
     title="AskDocs AI Service",
     version="1.0.0"
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # later replace with your Vercel URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
